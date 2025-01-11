9fb0ede0109bcee0d704be86d7d8d039745910bc
SamahSaeed400990 <121442474+SamahSaeed400990@users.noreply.github.com>
2025-01-11 09:41:42 +0200

    update

--- src/Components/Navbar/Navbar.jsx
+++ src/Components/Navbar/Navbar.jsx
@@ -63,6 +63,12 @@ export default function Navbar() {
           </NavLink>
           
         </li>
+        <li className="nav-item">
+          < NavLink className="nav-link position-relative" to="/login">
+              Logout
+          </NavLink>
+          
+        </li>
         
       
         
